5e2639cdaea232a27f83a23cf8bcc23279b059d1
alamin2811 <alaminbinmonsur@gmail.com>
2024-10-16 23:00:11 +0600

    modified

--- src/Component/Auth/ForgotPass.jsx
+++ src/Component/Auth/ForgotPass.jsx
@@ -11,12 +11,12 @@ const ForgotPass = () => {
             <p className="text-center mb-[30px]">Please enter the email address associated with your account and We will email you a link to reset your password.</p>
             <label className="relative">Email Adddress</label>
             <input
-              className="w-full h-[50px] px-[20px] py-[5px] text-[#111] text-[15px] font-[500] rounded-[6px] mb-[25px] mt-[5px] border-[1px] border-[rgba(0,0,0,0.15)]"
+              className="w-full h-[40px] px-[20px] py-[5px] text-[#111] text-[15px] font-[500] rounded-[6px] mb-[25px] mt-[5px] border-[1px] border-[rgba(0,0,0,0.15)]"
               id="email-basic" label="Email" name='email' variant="outlined"
             />
 
-            <button type='submit' variant="contained" className="bg-[#5985fd] opacity-[0.9] hover:opacity-[1] delay-75 h-[50px] w-full text-[16px] font-[600] rounded-[6px] text-white">Submit</button>
-            <Link href="/auth" className="bg-[#5985fd3a] hover:bg-[#5985fd59] flex items-center justify-center delay-75 h-[50px] w-full text-[16px] font-[600] rounded-[6px] mt-[20px] text-[#5985fd]">Back to Login</Link>
+            <button type='submit' variant="contained" className="bg-[#5985fd] opacity-[0.9] hover:opacity-[1] delay-75 h-[40px] w-full text-[16px] font-[600] rounded-[6px] text-white">Submit</button>
+            <Link href="/auth" className="bg-[#5985fd3a] hover:bg-[#5985fd59] flex items-center justify-center delay-75 h-[40px] w-full text-[16px] font-[600] rounded-[6px] mt-[20px] text-[#5985fd]">Back to Login</Link>
 
           </form>
         </div>
